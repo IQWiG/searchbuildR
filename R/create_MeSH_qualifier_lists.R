7f9bd70520baf8b20d1bee280b34d808b957b71a
Claudia Kapp <claudia.kapp@iqwig.de>
2023-05-11 09:04:46 +0200

    try fix env$popset not found error

--- R/create_MeSH_qualifier_lists.R
+++ R/create_MeSH_qualifier_lists.R
@@ -1,5 +1,5 @@
 create_MeSH_qualifier_lists <- function(testset){
-
+popset <- popset
 testset[["MeSH.Terms"]][["MeSH"]] <-  testset[["MeSH.Terms"]][["all_keywords"]] %>%
   filter(.data$MeSH %in% .env$popset[["MeSH.Terms"]][["MeSH"]]) %>%
   mutate(n = sum(frequency, na.rm = T))
